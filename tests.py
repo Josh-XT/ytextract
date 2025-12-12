d9a0deae054a797841c2781dedf49c6658870bf9
Josh XT <josh@devxt.com>
2025-12-12 09:18:57 -0500

    change video

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from pyyt.download_helper import download_video, download_captions
 
-TEST_VIDEO_URL = "https://www.youtube.com/watch?v=B00lazPU_yM"
+TEST_VIDEO_URL = "https://www.youtube.com/watch?v=aqz-KE-bpKQ"
 
 print("Download a Video from URL:")
 print(download_video(url=TEST_VIDEO_URL))
